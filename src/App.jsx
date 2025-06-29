b6b3337069c7be853fb90508eeb7d41ed472570a
VJ <vinjo1505@gmail.com>
2025-06-29 14:49:26 +0530

    added the background

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const geminiApiKey = env.GEMINI_API_KEY; 
-const genAI = new GoogleGenerativeAI(geminiApiKey);
+//const geminiApiKey = process.env.GEMINI_API_KEY; 
+//const genAI = new GoogleGenerativeAI(geminiApiKey);
 
-async function App() {
+function App() {
   const [count, setCount] = useState(0)
 
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  /*const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const prompt = "Explain how AI works in a few words";
-  const result = await model.generateContent(prompt);
+  const result = await model.generateContent(prompt) || 0;
   const response = await result.response;
-  console.log(response.text());
+  console.log(response.text());*/
 
   return (
     <>
-      <h1>{response || "Waiting..."}</h1>
+      <div className=''>Hello</div>
     </>
   )
 }
